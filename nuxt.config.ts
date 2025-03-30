e954954a1de75ff1c08ded3761f9f74ecf81f853
kirubaselvi <selvi.kiruba@moneyforward.co.jp>
2025-03-30 19:38:00 +0530

    feat:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: true,
-  nitro: {
-    preset: 'netlify'
-  },
   app: {
     head: {
       title: 'Money Forward India',
