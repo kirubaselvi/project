af9f218dd8df4ecf127b089fff9053144dee581b
kirubaselvi <selvi.kiruba@moneyforward.co.jp>
2025-03-30 19:19:02 +0530

    feat: add ssr config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true},
-  ssr: false,
+  ssr: true,
   nitro: {
     preset: 'netlify',
     prerender: {
