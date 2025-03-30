dc8d7c03dc6b67bbf4baf5519215b00083c401d1
kirubaselvi <selvi.kiruba@moneyforward.co.jp>
2025-03-30 19:21:39 +0530

    feat:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true},
+  devtools: { enabled: true },
   ssr: true,
   nitro: {
     preset: 'netlify',
-    prerender: {
-      failOnError: false,
-      crawlLinks: true
+    prerender: false,
+    output: {
+      dir: 'dist',
+      serverDir: '.output/server'
     }
   },
   app: {
